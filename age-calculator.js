e76679e6e4d44e0ffc260e7856c82b6cb105ae4e
alexannmill <alexannmill@gmail.com>
2022-08-03 15:02:29 -0700

    age Calculator

--- age-calculator.js
+++ age-calculator.js
@@ -2,8 +2,7 @@ let name = ""  ;
 let yearOfBirth ; 
 let currentYear = 2022;
 function ageCalculator(name, yearOfBirth, currentYear){
-  return name + " is " + (currentYear - yearOfBirth) + " years old."
+  return name + " is " + (currentYear - yearOfBirth) + " years old.";
 }
-console.log(ageCalculator())
 console.log(ageCalculator("Miranda", 1983, 2015));
 console.log(ageCalculator("Ferdinand", 1988, 2015));
