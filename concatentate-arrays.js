57aad03076d25a2dfffc745af7865e812d7778ae
alexannmill <alexannmill@gmail.com>
2022-08-09 04:10:25 +0000

    concat arrays

---
+++ concatentate-arrays.js
@@ -0,0 +1,8 @@
+function concat(loc1,loc2){
+  total = loc1.concat(loc2)
+  return total
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
