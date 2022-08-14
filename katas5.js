9e716084e7b7859c7c3ed2241c1ce26dd9be730c
alexannmill <alexannmill@gmail.com>
2022-08-14 03:19:51 +0000

    katas project 5

---
+++ katas5.js
@@ -0,0 +1,21 @@
+const urlEncode = function(text) {
+  let ulr = "";
+  if (text[0] !== " "){
+    ulr += text[0]
+  }
+  for (let i = 1; i < (text.length - 1); i++){
+    if(text[i] === " "){
+      ulr += "%20";
+    } else {
+      ulr += text[i];
+    }
+  }
+  if (text[text.length - 1] !== " "){
+    ulr += text[text.length - 1];
+  }
+  return ulr;
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
